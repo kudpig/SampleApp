9edc4c499413a0d1bdcb27f2d573597ec6216a2e
kudpig <shin.kudo.aa@gmail.com>
2021-02-05 12:03:23 +0900

    グラデーションを関数化

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -14,6 +14,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setUpGradation()
+        
+    }
+    
+    // グラデーション
+    func setUpGradation() {
         let gradientLeyer = CAGradientLayer()
         // CGRectでフレームそのものの座標を指定。xyは左上の位置。widthとheightはフレームのサイズ。
         gradientLeyer.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height / 2)
@@ -22,7 +29,6 @@ class ViewController: UIViewController {
         gradientLeyer.endPoint = CGPoint.init(x: 1, y: 1)
         view.layer.insertSublayer(gradientLeyer, at: 0)
     }
-
-
+    
 }
 
