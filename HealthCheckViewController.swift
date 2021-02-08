ba9812f2e961c47eb947a18a724e64cde6cea7cd
kudpig <shin.kudo.aa@gmail.com>
2021-02-08 19:15:43 +0900

    scrollViewの追加

--- HealthCheckViewController.swift
+++ HealthCheckViewController.swift
@@ -8,11 +8,21 @@
 import UIKit
 
 class HealthCheckViewController: UIViewController {
+    
+    let colors = Colors()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemGroupedBackground
+        
+        let scrollView = UIScrollView()
+        // frameは画面上のどこをスクロールビュー(窓)にするかを指定しており、contentSizeは中身のサイズ
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        scrollView.contentSize = CGSize(width: view.frame.size.width, height: 1000)
+        view.addSubview(scrollView)
+        
     }
     
 
