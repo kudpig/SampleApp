00c76220098f627fdd00a727a94406049e5cd11a
kudpig <shin.kudo.aa@gmail.com>
2021-02-11 15:47:34 +0900

    chart画面上部ボタン作成

--- SampleApp/ChartViewController.swift
+++ SampleApp/ChartViewController.swift
@@ -22,8 +22,31 @@ class ChartViewController: UIViewController {
         gradientLayer.endPoint = CGPoint.init(x: 1, y: 1)
         view.layer.insertSublayer(gradientLayer, at: 0) // レイヤはviewには載せらせない。階層0に差し込み。
         
+        // 上部ボタンのview(装飾)
+        let backButton = UIButton(type: .system)
+        backButton.frame = CGRect(x: 10, y: 30, width: 20, height: 20)
+        backButton.setImage(UIImage(named: "back"), for: .normal)
+        backButton.tintColor = colors.white
+        backButton.titleLabel?.font = .systemFont(ofSize: 20)
+        backButton.addTarget(self, action: #selector(backButtonAction), for: .touchUpInside)
+        view.addSubview(backButton)
+        
+        let nextButton = UIButton(type: .system)
+        nextButton.frame = CGRect(x: view.frame.size.width - 105, y: 30, width: 100, height: 20)
+        nextButton.setTitle("円グラフ", for: .normal)
+        nextButton.setTitleColor(colors.white, for: .normal)
+        nextButton.titleLabel?.font = .systemFont(ofSize: 20)
+        nextButton.addTarget(self, action: #selector(goCircle), for: .touchUpInside)
+        view.addSubview(nextButton)
     }
     
+    // 画面上部のボタン２つのアクション
+    @objc func backButtonAction() {
+        dismiss(animated: true, completion: nil)
+    }
+    @objc func goCircle() {
+        print("tappedNextButton")
+    }
 
     
 
